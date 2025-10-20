158ec2e8bcb74eba8e12bc76cf1ac28a8d0d31a4
MarcoBertaccini <marco.bertaccini9@gmail.com>
2025-10-20 17:58:25 +0200

    Set base path for Vite configuration
    Added base path configuration for the repository.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
+  base: '/zenith/', // <-- AGGIUNGI QUESTA RIGA (nome del tuo repository)
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
