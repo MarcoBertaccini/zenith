693599cd58f581b6643e923d95edf9939177b4c5
MarcoBertaccini <marco.bertaccini9@gmail.com>
2025-10-24 23:25:47 +0200

    Change base path in Vite config to root

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/zenith/', // <-- AGGIUNGI QUESTA RIGA (nome del tuo repository)
+  base: '/', // <-- AGGIUNGI QUESTA RIGA (nome del tuo repository)
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
